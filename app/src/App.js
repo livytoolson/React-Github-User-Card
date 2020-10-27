a6af2f7f60b67bba1b12d9fa64f113aa5849387f
Olivia Toolson <oliviatoolson@Olivias-MBP.lan>
2020-10-27 12:00:35 -0700

    have user info rendering to dom

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import axios from 'axios';
-// import './App.css';
+
+// import UserCard from './UserCard'
+import './App.css';
 
 class App extends React.Component {
   state = {
-    user: []
+    userData: {}
   }
 
   componentDidMount() {
     axios.get('https://api.github.com/users/livytoolson')
       .then(res => {
+        // console.log(res.data)
         this.setState({
-          user: res.data
+          userData: res.data
         })
       })
       .catch(err => {
@@ -22,7 +25,13 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
-
+        <h1>{this.state.userData.name}</h1>
+        <h5>{this.state.userData.login}</h5>
+        <p>{this.state.userData.location}</p>
+        <p>{this.state.userData.html_url}</p>
+        <p>{this.state.userData.followers}</p>
+        <p>{this.state.userData.following}</p>
+        <p>{this.state.userData.bio}</p>
       </div>
     )
   }
